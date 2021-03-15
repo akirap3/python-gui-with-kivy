e4aae78abc74ce9ea76718cecef24a472d105bea
akirapf3 <akirapf3@gmail.com>
2021-03-16 06:45:51 +0800

    #6

---
+++ kivy/builder.py
@@ -0,0 +1,34 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+Builder.load_file()
+
+class MyGridLayout(Widget):
+
+	name = ObjectProperty(None)
+	pizza = ObjectProperty(None)
+	color = ObjectProperty(None)
+	
+	def press(self):
+		name = self.name.text
+		pizza = self.pizza.text
+		color = self.color.text
+
+		self.name.text = ""
+		self.pizza.text = ""
+		self.color.text = ""
+
+		print(f'Hello {name}, you like {pizza}, and your favorite color is {color}') 
+		# Print it ot the scren
+		# self.add_widget(Label(text=f'Hello {name}, you like {pizza}, and your favorite color is {color}'))
+
+class MyApp(App):
+	def build(self):
+		return MyGridLayout()
+
+if __name__ == '__main__':
+	MyApp().run()
+
