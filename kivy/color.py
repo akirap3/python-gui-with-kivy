2b371bd0ab5c04c973321b23e22cc4e07105f217
akirapf3 <akirapf3@gmail.com>
2021-03-16 20:08:38 +0800

    #11

--- kivy/color.py
+++ kivy/color.py
@@ -4,44 +4,7 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 
-#Builder.load_file('whatever.kv')
-Builder.load_string("""
-<MyGridLayout>
-
-	name:name
-	pizza:pizza
-	color:color
-
-	GridLayout:
-		cols:1
-		size: root.width, root.height
-		GridLayout:
-			cols:2
-
-			Label:
-				text: 'Name'
-			TextInput:
-				id: name
-				multiline: False
-
-			Label:
-				text: 'Favorite Pizza'
-			TextInput:
-				id: pizza
-				multiline: False
-
-			Label:
-				text: 'Favotite Color'
-			TextInput:
-				id: color
-				multiline: False
-
-		Button:
-			text: "Submit"
-			font_size:32
-			on_press: root.press()
-
-	""")
+Builder.load_file('color.kv')
 
 class MyGridLayout(Widget):
 
