0506a7d12cfc6722e12412f54fffe502a5cda1c1
akirapf3 <akirapf3@gmail.com>
2021-03-18 18:07:32 +0800

    #32

---
+++ kivy/carousel.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+# from kivy.uix.slider import Slider
+
+Builder.load_file('carousel.kv')
+
+class MyLayout(Widget):
+	pass
+
+class AwesomeApp(App):
+	def build(self):
+		return MyLayout()
+
+if __name__ == '__main__':
+	AwesomeApp().run()
+
